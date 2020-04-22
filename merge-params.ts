60d027787b5774225c70462105241d4214274703
Umberto Pepato <umbo.pepato@gmail.com>
2020-04-22 16:10:09 +0200

    Rewrite params merging

--- merge-params.ts
+++ merge-params.ts
@@ -1,6 +1,6 @@
 import { ScriptParameters, FlagsObject } from "./types.ts";
 
-const normalizeFlags = (flags: string[] | FlagsObject | undefined): FlagsObject | undefined => {
+const normalizeFlags = (flags: string[] | FlagsObject | undefined) => {
   if (Array.isArray(flags)) {
     return flags.reduce((acc, val) => {
       acc[val] = true;
@@ -10,10 +10,19 @@ const normalizeFlags = (flags: string[] | FlagsObject | undefined): FlagsObject 
   return flags;
 };
 
-export const mergeParams = (parentParams: ScriptParameters, childParams: ScriptParameters): ScriptParameters => {
+export const mergeParams = (
+  parentParams: ScriptParameters,
+  childParams: ScriptParameters,
+): ScriptParameters => {
   const env = { ...parentParams.env, ...childParams.env };
-  const allow = { ...normalizeFlags(parentParams.allow), ...normalizeFlags(childParams.allow) };
-  const v8flags = { ...normalizeFlags(parentParams.v8flags), ...normalizeFlags(childParams.v8flags) };
+  const allow = {
+    ...normalizeFlags(parentParams.allow),
+    ...normalizeFlags(childParams.allow),
+  };
+  const v8flags = {
+    ...normalizeFlags(parentParams.v8flags),
+    ...normalizeFlags(childParams.v8flags),
+  };
   let res: ScriptParameters = { ...parentParams, ...childParams };
   if (env) res.env = env;
   if (allow) res.allow = allow;
