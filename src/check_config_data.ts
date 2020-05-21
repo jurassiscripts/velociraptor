e25522ca41e4cd53aa2b3ad9b8ed54aa8745e275
Umberto Pepato <umbo.pepato@gmail.com>
2020-05-21 14:23:05 +0200

    refactor: extract common logic between run_commands and export_scripts

---
+++ src/check_config_data.ts
@@ -0,0 +1,17 @@
+import { ConfigData } from "./load_config.ts";
+import { log } from "./logger.ts";
+
+export function checkConfigData(configData: ConfigData | null) {
+  if (!configData) {
+    throw new Error("No scripts file found.");
+  }
+  if (
+    !configData.config.scripts ||
+    Object.entries(configData.config.scripts).length < 1
+  ) {
+    log.warning(
+      "No scripts available.\nSee https://github.com/umbopepato/velociraptor for guidance on how to create scripts.",
+    );
+    Deno.exit();
+  }
+}
