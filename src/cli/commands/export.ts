9510a92cc07f3ae69d3ffa8fc696ff6bcaf8c36c
Umberto Pepato <umbo.pepato@gmail.com>
2020-05-24 19:45:08 +0200

    fix(cli): flags in additionalArgs consumed by vr
    Fixes #26

--- src/cli/commands/export.ts
+++ src/cli/commands/export.ts
@@ -4,13 +4,11 @@ import { ConfigData } from "../../load_config.ts";
 import { exportScripts } from "../../export_scripts.ts";
 
 export class ExportCommand extends BaseCommand {
-  private helpCommand = new HelpCommand(this);
-
   constructor(private configData: ConfigData | null) {
     super();
     this.description("Export one or more scripts as executable files")
       .type("scriptid", new ScriptIdType(this.configData))
-      .arguments("[scripts...:string:scriptid]")
+      .arguments("[scripts...:scriptid]")
       .option(
         "-o, --out-dir [dir:string]",
         "The folder where the scripts will be exported",
@@ -18,9 +16,5 @@ export class ExportCommand extends BaseCommand {
       .action(async (options, scripts: string[]) => {
         await exportScripts(this.configData, scripts, options.outDir);
       });
-  }
-
-  getHelpCommand(): HelpCommand {
-    return this.helpCommand;
   }
 }
