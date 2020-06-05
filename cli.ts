23e670d26b47dec206ad4e563a1c69f25de26a34
Divy Srivastava <dj.srivastava23@gmail.com>
2020-06-05 14:22:38 +0530

    chores: fmt

--- cli.ts
+++ cli.ts
@@ -2,7 +2,7 @@ import { loadConfig } from "./src/load_config.ts";
 import { VrCommand } from "./src/cli/commands/vr.ts";
 
 if (import.meta.main) {
-  loadConfig().then(async conf => {
+  loadConfig().then(async (conf) => {
     await new VrCommand(conf)
       .parse(Deno.args);
   });
