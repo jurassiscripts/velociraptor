726a66c133e33dc4d2d4ab7438a812615b11b2d1
Umberto Pepato <umbo.pepato@gmail.com>
2020-10-30 09:38:46 +0100

    fix: deno 1.5.0 incompatibility

--- deps.ts
+++ deps.ts
@@ -1,22 +1,22 @@
 export { default as levenshtein } from "https://deno.land/x/levenshtein@v1.0.1/mod.ts";
-export { parse as parseYaml } from "https://deno.land/std@0.70.0/encoding/yaml.ts";
-export { moveSync } from "https://deno.land/std@0.70.0/fs/move.ts";
-export { existsSync } from "https://deno.land/std@0.70.0/fs/exists.ts";
-export { ensureDirSync } from "https://deno.land/std@0.70.0/fs/ensure_dir.ts";
-export * as path from "https://deno.land/std@0.70.0/path/mod.ts";
-export * as logger from "https://deno.land/std@0.70.0/log/mod.ts";
+export { parse as parseYaml } from "https://deno.land/std@0.75.0/encoding/yaml.ts";
+export { moveSync } from "https://deno.land/std@0.75.0/fs/move.ts";
+export { existsSync } from "https://deno.land/std@0.75.0/fs/exists.ts";
+export { ensureDirSync } from "https://deno.land/std@0.75.0/fs/ensure_dir.ts";
+export * as path from "https://deno.land/std@0.75.0/path/mod.ts";
+export * as logger from "https://deno.land/std@0.75.0/log/mod.ts";
 export {
   blue,
   bold,
   gray,
   red,
   yellow,
-} from "https://deno.land/std@0.70.0/fmt/colors.ts";
-export { ConsoleHandler } from "https://deno.land/std@0.70.0/log/handlers.ts";
-export { LogRecord } from "https://deno.land/std@0.70.0/log/logger.ts";
-export { LevelName } from "https://deno.land/std@0.70.0/log/levels.ts";
+} from "https://deno.land/std@0.75.0/fmt/colors.ts";
+export { ConsoleHandler } from "https://deno.land/std@0.75.0/log/handlers.ts";
+export { LogRecord } from "https://deno.land/std@0.75.0/log/logger.ts";
+export type { LevelName } from "https://deno.land/std@0.75.0/log/levels.ts";
 export {
   Command,
   CompletionsCommand,
   StringType,
-} from "https://deno.land/x/cliffy@v0.14.3/command/mod.ts";
+} from "https://deno.land/x/cliffy@v0.15.0/command/mod.ts";
